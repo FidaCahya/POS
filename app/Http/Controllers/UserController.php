77b41520a162686a4ad79ed474fc3fb320bf9e23
FidaCahya <fidaacahya@gmail.com>
2024-09-14 22:56:52 +0700

    Praktikum 6 - JS 3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function showProfile($id, $name)
-    {
-        return view('user.profile', ['id' => $id, 'name' => $name]);
-    }
+    //JS 2
+    // public function showProfile($id, $name)
+    // {
+    //     return view('user.profile', ['id' => $id, 'name' => $name]);
+    // }
+
+    // JS 3 - Praktikum 6
+    
+        public function index()
+        {
+            // tambah data user dengan Eloquent Model
+            //Praktkum 6 no 8 JS 3
+            // $data = [
+            //     'username' => 'customer-3',
+            //     'nama' => 'Pelanggan',
+            //     'password' => Hash::make('12345'),
+            //     'level_id' => 3
+
+            // ];
+            //UserModel::insert($data);   //tambahkan data ke tabel m_user
+
+
+            //Praktikum 6 no 10 JS 3
+            $data = [
+                'nama' => 'Pelanggan Pertama',
+            ];
+            UserModel::where('username', 'customer-1')->update($data); //update data user
+            
+            //coba model akses UserModul
+            $user = UserModel::all(); //ambil semua data dr tabel m_user
+            return view('user', ['data' => $user]);
+        }
+    
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -25,15 +25,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::prefix('category')->group(function() {
-    Route::get('food-beverage', [ProductController::class, 'foodBeverage'])->name('category.food-beverage');
-    Route::get('beauty-health', [ProductController::class, 'beautyHealth'])->name('category.beauty-health');
-    Route::get('home-care', [ProductController::class, 'homeCare'])->name('category.home-care');
-    Route::get('baby-kid', [ProductController::class, 'babyKid'])->name('category.baby-kid');
+// Route::prefix('category')->group(function() {
+//     Route::get('food-beverage', [ProductController::class, 'foodBeverage'])->name('category.food-beverage');
+//     Route::get('beauty-health', [ProductController::class, 'beautyHealth'])->name('category.beauty-health');
+//     Route::get('home-care', [ProductController::class, 'homeCare'])->name('category.home-care');
+//     Route::get('baby-kid', [ProductController::class, 'babyKid'])->name('category.baby-kid');
 
-});
+// });
  
-Route::get('user/{id}/name/{name}', [UserController::class, 'showProfile'])->name('user.profile');
+//Route::get('user/{id}/name/{name}', [UserController::class, 'showProfile'])->name('user.profile');
 
 Route::get('transaction', [TransactionController::class, 'showTransaction'])->name('transaction.index');
 
@@ -42,3 +42,6 @@ Route::get('/level', [LevelController::class, 'index']);
 
 //JS 3 | Praktikum 5
 Route::get('/kategori', [KategoriController::class, 'index']);
+
+//JS 3 | Praktikum 6
+Route::get ('/user', [UserController::class, 'index']);
