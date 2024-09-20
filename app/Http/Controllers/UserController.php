b6fd11cd5e263553b9128b4b1941393e32349edd
FidaCahya <fidaacahya@gmail.com>
2024-09-20 12:02:02 +0700

    Jobsheet 04 - Praktikum2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -188,50 +188,60 @@ class UserController extends Controller
             // dd($user->wasChanged(['nama', 'username']));//true
         
             //Praktikum 2.5 no 2 JS 4
-            $user = UserModel::all();
-            return view('user', ['data'=> $user]);
+            //$user = UserModel::all();
+            //return view('user', ['data'=> $user]);
+
+            //Praktikum 2.7 no 2 JS 4
+            // $user = UserModel::with('level')->get();
+            // dd($user);
+
+            //Praktikum 2.7 no 4 JS 4
+            $user = UserModel::with('level')->get();
+            return view('user', ['data' => $user]);
+
         }
-        //Praktikum 2.6 no 6 JS 4
-         public function tambah () {
-             return view('user_tambah');
-         }
+        // //Praktikum 2.6 no 6 JS 4
+        //  public function tambah () {
+        //      return view('user_tambah');
+        //  }
 
-        //Praktikum 2.6 no 8 JS 4
-        public function tambah_simpan (Request $request) {
-            UserModel::create([
-                'username' => $request->username,
-                'nama' => $request->nama,
-                'password' => Hash::make($request->password),
-                'level_id' => $request->level_id
-            ]);
-            return redirect('/user');
-        }
+        // //Praktikum 2.6 no 8 JS 4
+        // public function tambah_simpan (Request $request) {
+        //     UserModel::create([
+        //         'username' => $request->username,
+        //         'nama' => $request->nama,
+        //         'password' => Hash::make($request->password),
+        //         'level_id' => $request->level_id
+        //     ]);
+        //     return redirect('/user');
+        // }
 
-        public function ubah($id){
-            $user = UserModel::find($id);
-            return view('user_ubah', ['data' =>$user]);
-        }
+        // public function ubah($id){
+        //     $user = UserModel::find($id);
+        //     return view('user_ubah', ['data' =>$user]);
+        // }
 
-        //Praktikum 2.6 no 16 JS 4
-        public function ubah_simpan($id, Request $request){
-            $user = UserModel::find($id); 
+        // //Praktikum 2.6 no 16 JS 4
+        // public function ubah_simpan($id, Request $request){
+        //     $user = UserModel::find($id); 
 
-            $user->username = $request->username;
-            $user->nama = $request->nama;
-            $user->password = Hash::make('$request->password');
-            $user->level_id = $request->level_id;
+        //     $user->username = $request->username;
+        //     $user->nama = $request->nama;
+        //     $user->password = Hash::make('$request->password');
+        //     $user->level_id = $request->level_id;
 
-            $user->save();
+        //     $user->save();
                 
-            return redirect('/user');
-        }
-        public function hapus($id){
-            $user = UserModel::find($id);
-            $user->delete();
+        //     return redirect('/user');
+        // }
+        // public function hapus($id){
+        //     $user = UserModel::find($id);
+        //     $user->delete();
 
-            return redirect('/user');
-        }
+        //     return redirect('/user');
+        // }
 
+        
 
     
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,26 +5,17 @@
 </head>
 <body>
     <h1>Data User</h1>
-    <a href= "{{url("/user/tambah")}}">+ Tambah User</a>
-    
-    <!-- Menampilkan jumlah pengguna -->
-    {{-- <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>Jumlah Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $totalUsers }}</td>
-        </tr>
-    </table>
-    <br> --}}
+    {{-- <a href= "{{url("/user/tambah")}}">+ Tambah User</a> --}}
 
-    <!-- Menampilkan daftar pengguna -->
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <th>Kode Level</th>
+            <th>Nama Level</th>
+            <th>Aksi</th>
         </tr>
 
         @foreach ($data as $d)
@@ -33,6 +24,8 @@
             <td>{{ $d->username }}</td>
             <td>{{ $d->nama }}</td>
             <td>{{ $d->level_id }}</td>
+            <td>{{ $d->level->level_kode }}</td>
+            <td>{{ $d->level->level_nama }}</td>
             <td>
                 <a href="{{ url('/user/ubah/' . $d->user_id) }}">Ubah</a> | 
                 <a href="{{ url('/user/hapus/' . $d->user_id) }}">Hapus</a>
