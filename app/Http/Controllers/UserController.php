e0b80f35582c8c40e4469a47d2c2a2604547d614
FidaCahya <fidaacahya@gmail.com>
2024-09-27 22:46:10 +0700

    Jobsheet 05 - Praktikum 04

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -259,12 +259,19 @@ class UserController extends Controller
             ];
 
             $activeMenu = 'user'; //set menu yag sedang aktif
-            return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+            
+            $level = LevelModel::all(); //ambil data level untuk filter
+            return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
         }
+
         public function list(Request $request)
         {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
-
+        
+        //Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
         return DataTables::of($users)
             ->addIndexColumn()
             ->addColumn('aksi', function ($user) {
