983b32d1808e6f381d63bc3e9d989c1d3c7f779e
FidaCahya <fidaacahya@gmail.com>
2024-11-01 00:15:47 +0700

    Jobsheet 10 - Praktikum 3 Membuat RESTful API Logout

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', App\http\Controllers\Api\RegisterController:: class)->name('register');
 Route::post('/login', App\http\Controllers\Api\LoginController:: class)->name('login');
+Route::post('/logout', App\http\Controllers\Api\LogoutController:: class)->name('logout');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
      return $request->user();
 });
-
