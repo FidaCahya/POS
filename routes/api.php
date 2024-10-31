565425b9bae6aa1433dfb9040b1950f4376d975c
FidaCahya <fidaacahya@gmail.com>
2024-10-31 22:50:09 +0700

    Josheet 10 - Praktikum 2 Membuat RESTful API Login

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', App\http\Controllers\Api\RegisterController:: class)->name('register');
+Route::post('/login', App\http\Controllers\Api\LoginController:: class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+     return $request->user();
+});
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
