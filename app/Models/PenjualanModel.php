3823aedfa7ead1d3d6f5130a88c12eb88649c0f9
FidaCahya <fidaacahya@gmail.com>
2024-11-07 11:13:04 +0700

    JS11-Tugas Eloquent Accessor tabel t_penjualan

--- app/Models/PenjualanModel.php
+++ app/Models/PenjualanModel.php
@@ -3,6 +3,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 class PenjualanModel extends Model
 {
     use HasFactory;
@@ -14,9 +15,17 @@ class PenjualanModel extends Model
         'pembeli',  
         'penjualan_kode',
         'penjualan_tanggal',
+        'image',
     ];
     public function user(): BelongsTo
-{
+    {
     return $this->belongsTo(UserModel::class, 'user_id', 'user_id');
-}
+    }
+    public function image(): Attribute
+    {
+        return Attribute::make(
+            get:fn($image) =>url('/storage/posts' . $image),
+        );
+        
+    }
 }
